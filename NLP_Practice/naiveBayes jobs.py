618c156a8c182f34135b5ac3489377309a00ce9e
Gopu2001 <41317321+Gopu2001@users.noreply.github.com>
2020-10-13 13:56:17 -0700

    submitting Weekend #2's work and deleting credentials

---
+++ NLP_Practice/naiveBayes jobs.py
@@ -0,0 +1,37 @@
+import numpy as np
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
+import pandas as pd
+from collections import Counter
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfTransformer
+import sqlite3 as sql
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+
+src = '..\\jobs.db'
+
+conn, cur = None, None
+try:
+    conn = sql.connect(src)
+except sql.Error as e:
+    print(e)
+    sys.exit(1)
+
+df = pd.read_sql_query("select title, yes_no from jobs", conn)
+keys = ['title', 'yes_no']
+
+X_train, X_test, y_train, y_test = train_test_split(df['title'], df['yes_no'], random_state=1)
+cv = CountVectorizer(strip_accents='ascii', token_pattern=u'(?ui)\\b\\w*[a-z]+\\w*\\b', lowercase=True, stop_words='english')
+
+X_train_cv = cv.fit_transform(X_train)
+X_test_cv  = cv.fit_transform(X_test)
+word_freq_df = pd.DataFrame(X_train_cv.toarray(), columns=cv.get_feature_names())
+
+top_words_df = pdf.DataFrame(word_freq.sum()).sortValues(0, ascending=False)
+naive_bayes = MultinomialNB()
+naive_bayes.fit(X_train_cv, y_train)
+predictions = naive_bayes.predict(X_test_cv)
+
+print('Accuracy score:', accuracy_score(y_test, predictions))
+print('Precision score:', precision_score(y_test, predictions))
+print('Recall score:', recall_score(y_test, predictions))
